14c06eabf6c0cbb143037376dda0b2e531937fc1
notha99y <renjie.tan91@gmail.com>
2018-12-18 18:02:03 +0800

    added quantitative_summarized. update titanic.ipynb with quantitative analysis and feature selection. explained dashboarding practices

--- helper.py
+++ helper.py
@@ -41,5 +41,38 @@ def categorical_summarized(dataframe, x=None, y=None, hue=None, palette='Set1', 
     plt.show()
 
 
+def quantitative_summarized(dataframe, x=None, y=None, hue=None, palette='Set1', ax=None, verbose=True, swarm=False):
+    '''
+    Helper function that gives a quick summary of quantattive data
+
+    Arguments
+    =========
+    dataframe: pandas dataframe
+    x: str. horizontal axis to plot the labels of categorical data (usually the target variable)
+    y: str. vertical axis to plot the quantitative data
+    hue: str. if you want to compare it another categorical variable (usually the target variable if x is another variable)
+    palette: array-like. Colour of the plot
+    swarm: if swarm is set to True, a swarm plot would be overlayed
+
+    Returns
+    =======
+    Quick Stats of the data and also the box plot of the distribution
+    '''
+    series = dataframe[y]
+    print(series.describe())
+    print('mode: ', series.mode())
+    if verbose:
+        print('='*80)
+        print(series.value_counts())
+
+    sns.boxplot(x=x, y=y, hue=hue, data=dataframe, palette=palette, ax=ax)
+
+    if swarm:
+        sns.swarmplot(x=x, y=y, hue=hue, data=dataframe,
+                      palette=palette, ax=ax)
+
+    plt.show()
+
+
 if __name__ == "__main__":
     pass
